da4aaf8a85caafdcf629bc0125c78e5a4f3c6e52
Yu-Jie Lin <livibetter@gmail.com>
2015-10-21 12:07:57 +0800

    fix --brick option (#6)

--- legofy/cli.py
+++ legofy/cli.py
@@ -10,4 +10,5 @@ import legofy
 def main(brick, image, output):
     if not brick:
         here = os.path.abspath(os.path.dirname(__file__))
-    legofy.main(image, brick=os.path.join(here, 'bricks', 'brick.png'))
+        brick = os.path.join(here, 'bricks', 'brick.png')
+    legofy.main(image, brick=brick)
