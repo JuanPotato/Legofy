d6f151e1b2d2c83cbf29da8fa1e3e9b04fef320a
Yu-Jie Lin <livibetter@gmail.com>
2015-10-21 11:51:20 +0800

    Allow directly running without installation (#6)
        python -m legofy.cli

--- legofy/cli.py
+++ legofy/cli.py
@@ -11,3 +11,7 @@ def main(brick, image, output):
     if not brick:
         here = os.path.abspath(os.path.dirname(__file__))
     legofy.main(image, brick=os.path.join(here, 'bricks', 'brick.png'))
+
+
+if __name__ == '__main__':
+    main()
