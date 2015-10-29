468e4d9271d7ef69163e80672f51a97457020dba
Scott Wentz <scott.wentz@epitech.eu>
2015-10-29 12:54:30 +0100

    fixed build error

--- tests/test_legofy.py
+++ tests/test_legofy.py
@@ -4,5 +4,5 @@ import os
 here = os.path.abspath(os.path.dirname(__file__))
 # print(here)
 # print(legofy)
-legofy.main(os.path.join(here, 'image.jpg'), os.path.join(here, 'brick.png'))
+legofy.main(os.path.join(here, 'image.jpg'), False, os.path.join(here, 'brick.png'))
 # legofy.main(image, brick=os.path.join(here, 'bricks', 'brick.png'))
