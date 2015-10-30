8027ea5ab026ba68643ce8c85e31a05383ae4902
grisu48 <grisu42@gmail.com>
2015-10-30 09:08:59 +0100

    Added --bricks as program argument

--- legofy/cli.py
+++ legofy/cli.py
@@ -5,13 +5,15 @@ import legofy
 @click.command()
 @click.option('--brick', default=None, type=click.Path(dir_okay=False,
                                                        exists=True))
+@click.option('--bricks', default=30, required=False, type=int)
 @click.argument('image', required=True)
 @click.argument('output', default=None, required=False)
-def main(brick, image, output):
+def main(brick, image, output, bricks):
     if not brick:
         here = os.path.abspath(os.path.dirname(__file__))
         brick = os.path.join(here, 'bricks', 'brick.png')
-    legofy.main(image, brick=brick)
+    if not bricks : bricks = 30
+    legofy.main(image, brick=brick, bricks=bricks)
 
 if __name__ == '__main__':
     main()
