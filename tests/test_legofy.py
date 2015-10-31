7547574a8fc42c484a0ccf3580780f8ed3116dbb
Jacob <jcwilks21396@gmail.com>
2015-10-31 03:14:09 -0400

    Organized variables
    For the example went ahead and organized some of the variables, making
    it easier to edit for people who may want to test it out.

--- tests/test_legofy.py
+++ tests/test_legofy.py
@@ -1,8 +1,14 @@
 import legofy
 import os
 
+brick_img = 'brick.png'
+target_img = 'image.jpg'
+
 here = os.path.abspath(os.path.dirname(__file__))
+target_img_path = os.path.join(here, target_img)
+brick_img_path = os.path.join(here, brick_img)
+
 # print(here)
 # print(legofy)
-legofy.main(os.path.join(here, 'image.jpg'), os.path.join(here, 'brick.png'))
+legofy.main(target_img_path, brick_img_path)
 # legofy.main(image, brick=os.path.join(here, 'bricks', 'brick.png'))
