c618fe4fdf577b9c5a6d6ef7047ca6b416ab176c
Tomcu <Tomcu@users.noreply.github.com>
2015-10-31 14:17:50 -0400

    Fix unit test.

--- tests/test_legofy.py
+++ tests/test_legofy.py
@@ -4,5 +4,5 @@ import os
 here = os.path.abspath(os.path.dirname(__file__))
 # print(here)
 # print(legofy)
-legofy.main(os.path.join(here, 'image.jpg'), os.path.join(here, 'brick.png'))
+legofy.main(os.path.join(here, 'image.jpg'), os.path.join(here, 'brick.png'), os.path.join(here, 'legos.csv'))
 # legofy.main(image, brick=os.path.join(here, 'bricks', 'brick.png'))
