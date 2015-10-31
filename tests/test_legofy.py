0dbead44e99c2e0a7459afebd030b71db661fc1d
Tomcu <Tomcu@users.noreply.github.com>
2015-10-31 18:42:38 -0400

    Removed copy of legos.csv, make test use it from legofy directory. While there clean up the paths a bit, use bricks from same location too.

--- tests/test_legofy.py
+++ tests/test_legofy.py
@@ -2,7 +2,11 @@ import legofy
 import os
 
 here = os.path.abspath(os.path.dirname(__file__))
+legofyDir = os.path.join(here, os.pardir, "legofy")
+paletteDir = os.path.join(legofyDir, "palettes")
+brickDir = os.path.join(legofyDir, "bricks")
+
 # print(here)
 # print(legofy)
-legofy.main(os.path.join(here, 'image.jpg'), os.path.join(here, 'brick.png'), os.path.join(here, 'legos.csv'))
+legofy.main(os.path.join(here, 'image.jpg'), os.path.join(brickDir, 'brick.png'), os.path.join(paletteDir, 'legos.csv'))
 # legofy.main(image, brick=os.path.join(here, 'bricks', 'brick.png'))
