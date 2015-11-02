f6889075ee8c37099aac873b9122f6acb54720c8
JuanPotato <hasantiny@gmail.com>
2015-11-02 17:44:45 -0500

    forgot this. Sorry travis

--- tests/test_legofy.py
+++ tests/test_legofy.py
@@ -8,22 +8,6 @@ class LegofyTestCase(unittest.TestCase):
     def test_full_process(self):
         here = os.path.abspath(os.path.dirname(__file__))
         image = os.path.join(here, 'image.jpg')
-        brick = os.path.join(here, 'brick.png')
-        legofy.main(image, brick, output='lego_image.png')
+        legofy.main(image, output='lego_image.png')
         expected = os.path.join(here, 'lego_image.png')
         self.assertTrue(os.path.exists(expected))
-
-    def test_over_under_min(self):
-        value = -200
-        result = legofy.overUnder(value)
-        self.assertEqual(result, -100)
-
-    def test_over_under_max(self):
-        value = 200
-        result = legofy.overUnder(value)
-        self.assertEqual(result, 100)
-
-    def test_over_under_mid(self):
-        value = 55
-        result = legofy.overUnder(value)
-        self.assertEqual(result, value)
