2c760eb12f883e1fc05b9fe5be8e4ace4e3c3a06
Nate Gallinger <nigallin@gmail.com>
2015-11-03 17:13:59 -0800

    added scale argument

--- legofy/cli.py
+++ legofy/cli.py
@@ -6,17 +6,18 @@ import legofy
 @click.command()
 @click.option('--brick', default=None, type=click.Path(dir_okay=False, exists=True))
 @click.argument('image', required=True)
+@click.argument('scale', required=True)
 @click.argument('output', default=None, required=False)
-def main(brick, image, output):
+def main(image, scale, brick, output):
     '''Main entry point'''
     if brick and output:
-        legofy.main(image, brick=brick, output=output)
+        legofy.main(image, scale, brick=brick, output=output)
     elif brick:
-        legofy.main(image, brick=brick)
+        legofy.main(image, scale, brick=brick)
     elif output:
-        legofy.main(image, output=output)
+        legofy.main(image, scale, output=output)
     else:
-        legofy.main(image)
+        legofy.main(image, scale)
 
 if __name__ == '__main__':
     main()
