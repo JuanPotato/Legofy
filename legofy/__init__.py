d53a3442823b75f047ac72a1949d9d27af218e00
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2015-11-14 06:11:21 +0100

    Improve performance and verbose

--- legofy/__init__.py
+++ legofy/__init__.py
@@ -7,70 +7,78 @@ import sys
 import os
 
 '''http://www.brickjournal.com/files/PDFs/2010LEGOcolorpalette.pdf'''
-palette_solid = (
-    0xfe, 0xc4, 0x01,
-    0xe7, 0x64, 0x19,
-    0xde, 0x01, 0x0e,
-    0xde, 0x38, 0x8b,
-    0x01, 0x58, 0xa8,
-    0x01, 0x7c, 0x29,
-    0x95, 0xb9, 0x0c,
-    0x5c, 0x1d, 0x0d,
-    0xd6, 0x73, 0x41,
-    0xf4, 0xf4, 0xf4,
-    0xff, 0xff, 0x99,
-    0xee, 0x9d, 0xc3,
-    0x87, 0xc0, 0xea,
-    0x01, 0x96, 0x25,
-    0xd9, 0xbb, 0x7c,
-    0xf5, 0xc1, 0x89,
-    0xe4, 0xe4, 0xda,
-    0xf4, 0x9b, 0x01,
-    0x9c, 0x01, 0xc6,
-    0x48, 0x8c, 0xc6,
-    0x5f, 0x75, 0x8c,
-    0x60, 0x82, 0x66,
-    0x8d, 0x75, 0x53,
-    0xa8, 0x3e, 0x16,
-    0x9c, 0x92, 0x91,
-    0x80, 0x09, 0x1c,
-    0x2d, 0x16, 0x78,
-    0x01, 0x26, 0x42,
-    0x01, 0x35, 0x17,
-    0xaa, 0x7e, 0x56,
-    0x4d, 0x5e, 0x57,
-    0x31, 0x10, 0x07,
-)
+palette_solid = {
+    "024": [0xfe, 0xc4, 0x01],
+    "106": [0xe7, 0x64, 0x19],
+    "021": [0xde, 0x01, 0x0e],
+    "221": [0xde, 0x38, 0x8b],
+    "023": [0x01, 0x58, 0xa8],
+    "028": [0x01, 0x7c, 0x29],
+    "119": [0x95, 0xb9, 0x0c],
+    "192": [0x5c, 0x1d, 0x0d],
+    "018": [0xd6, 0x73, 0x41],
+    "001": [0xf4, 0xf4, 0xf4],
+    "026": [0x02, 0x02, 0x02],
+    "226": [0xff, 0xff, 0x99],
+    "222": [0xee, 0x9d, 0xc3],
+    "212": [0x87, 0xc0, 0xea],
+    "037": [0x01, 0x96, 0x25],
+    "005": [0xd9, 0xbb, 0x7c],
+    "283": [0xf5, 0xc1, 0x89],
+    "208": [0xe4, 0xe4, 0xda],
+    "191": [0xf4, 0x9b, 0x01],
+    "124": [0x9c, 0x01, 0xc6],
+    "102": [0x48, 0x8c, 0xc6],
+    "135": [0x5f, 0x75, 0x8c],
+    "151": [0x60, 0x82, 0x66],
+    "138": [0x8d, 0x75, 0x53],
+    "038": [0xa8, 0x3e, 0x16],
+    "194": [0x9c, 0x92, 0x91],
+    "154": [0x80, 0x09, 0x1c],
+    "268": [0x2d, 0x16, 0x78],
+    "140": [0x01, 0x26, 0x42],
+    "141": [0x01, 0x35, 0x17],
+    "312": [0xaa, 0x7e, 0x56],
+    "199": [0x4d, 0x5e, 0x57],
+    "308": [0x31, 0x10, 0x07]
+}
 
-palette_transparent = (
-    0xf9, 0xef, 0x69,
-    0xec, 0x76, 0x0e,
-    0xe7, 0x66, 0x48,
-    0xe0, 0x2a, 0x29,
-    0xee, 0x9d, 0xc3,
-    0x9c, 0x95, 0xc7,
-    0xb6, 0xe0, 0xea,
-    0x50, 0xb1, 0xe8,
-    0xce, 0xe3, 0xf6,
-    0x63, 0xb2, 0x6e,
-    0x99, 0xff, 0x66,
-    0xf1, 0xed, 0x5b,
-    0xa6, 0x91, 0x82,
-    0xee, 0xee, 0xee,
-)
+palette_transparent = {
+    "044": [0xf9, 0xef, 0x69],
+    "182": [0xec, 0x76, 0x0e],
+    "047": [0xe7, 0x66, 0x48],
+    "041": [0xe0, 0x2a, 0x29],
+    "113": [0xee, 0x9d, 0xc3],
+    "126": [0x9c, 0x95, 0xc7],
+    "042": [0xb6, 0xe0, 0xea],
+    "043": [0x50, 0xb1, 0xe8],
+    "143": [0xce, 0xe3, 0xf6],
+    "048": [0x63, 0xb2, 0x6e],
+    "311": [0x99, 0xff, 0x66],
+    "049": [0xf1, 0xed, 0x5b],
+    "111": [0xa6, 0x91, 0x82],
+    "040": [0xee, 0xee, 0xee]
+}
 
-palette_effects = (
-    0x8d, 0x94, 0x96,
-    0xaa, 0x7f, 0x2e,
-    0x49, 0x3f, 0x3b,
-    0xfe, 0xfc, 0xd5,
-)
+palette_effects = {
+    "131": [0x8d, 0x94, 0x96],
+    "297": [0xaa, 0x7f, 0x2e],
+    "148": [0x49, 0x3f, 0x3b],
+    "294": [0xfe, 0xfc, 0xd5]
+}
 
-palette_mono = (
-    0xf4, 0xf4, 0xf4,
-    0x01, 0x01, 0x01,
-)
+palette_mono = {
+    "001": [0xf4, 0xf4, 0xf4],
+    "026": [0x02, 0x02, 0x02]
+}
 
+palette_complete = {
+    "all":          [0x02, 0x02, 0x02],
+    "solid":        [0x02, 0x02, 0x02],
+    "transparent":  [0xa6, 0x91, 0x82],
+    "effects":      [0x49, 0x3f, 0x3b],
+    "mono":         [0x02, 0x02, 0x02]
+}
 
 def iter_frames(image_to_iter):
     '''Function that iterates over the gif's frames'''
@@ -111,27 +119,55 @@ def make_lego_brick(brick_image, overlay_red, overlay_green, overlay_blue):
 
 def make_lego_image(base_image, brick_image, palette):
     '''Create a lego version of an image from an image'''
+    color_map = {}
+
     base_width, base_height = base_image.size
     brick_width, brick_height = brick_image.size
-    base_poa = base_image.load()
+
+    base_poa = base_image.convert('RGB')
+
+    if palette:
+        base_poa = make_lego_palette(base_poa, palette, False)
+        base_poa = base_poa.convert('RGB')
 
     lego_image = Image.new("RGB", (base_width * brick_width, base_height * brick_height), "white")
 
     for x in range(base_width):
         for y in range(base_height):
-
-            if palette:
-                tmp_color = '#%02x%02x%02x' % base_poa[x, y]
-                tmp_image = Image.new("RGB", (1, 1), tmp_color)
-                tmp_image = make_lego_palette(tmp_image, palette)
-                tmp_image = tmp_image.convert('RGB')
-                bp = tmp_image.getpixel((0, 0))
+            bp = base_poa.getpixel((x, y))
+            bph = '#%02x%02x%02x' % bp
+            if bph in color_map:
+                color_map[bph] += 1
             else:
-                bp = base_poa[x, y]
+                color_map[bph] = 1
 
             lego_image.paste(make_lego_brick(brick_image, bp[0], bp[1], bp[2]), (x * brick_width, y * brick_height, (x + 1) * brick_width, (y + 1) * brick_height))
 
-    del base_poa
+    total = 0
+
+    if (palette == 'solid'):
+        palette_selected = dict(palette_solid.items())
+    elif (palette == 'transparent'):
+        palette_selected = dict(palette_transparent.items())
+    elif (palette == 'effects'):
+        palette_selected = dict(palette_effects.items())
+    elif (palette == 'mono'):
+        palette_selected = dict(palette_mono.items())
+    else:
+        palette_selected = dict(palette_solid.items() + palette_transparent.items() + palette_effects.items())
+
+    for color in sorted(color_map, key=color_map.get, reverse=True):
+        search_color = [int(color[1:3], 16), int(color[3:5], 16), int(color[5:7], 16)]
+
+        for code, value in palette_selected.iteritems():
+            if value == search_color:
+                print 'Brick LEGO ' + code,
+                break
+
+        print '(' + color + ') :', color_map[color]
+        total += color_map[color]
+
+    print "Total:", total
 
     return lego_image
 
@@ -171,23 +207,27 @@ def get_new_size(base_image, brick_image, bricks=None):
     return new_size
 
 
-def make_lego_palette(image, palette):
+def make_lego_palette(image, palette, dither=False):
+    palette_use = ()
     palette_image = Image.new("P", (1, 1))
 
     if (palette == 'solid'):
-        palette_use = palette_solid
+        palette_selected = dict(palette_solid.items())
     elif (palette == 'transparent'):
-        palette_use = palette_transparent
+        palette_selected = dict(palette_transparent.items())
     elif (palette == 'effects'):
-        palette_use = palette_effects
+        palette_selected = dict(palette_effects.items())
     elif (palette == 'mono'):
-        palette_use = palette_mono
+        palette_selected = dict(palette_mono.items())
     else:
-        palette_use = palette_solid + palette_transparent + palette_effects
+        palette_selected = dict(palette_solid.items() + palette_transparent.items() + palette_effects.items())
 
-    palette_image.putpalette(palette_use + (0x02, 0x02, 0x02) * (256 - len(palette_use)/3))
+    for color in palette_selected:
+        palette_use = palette_use + tuple(palette_selected[color])
 
-    return image.convert("RGB").quantize(palette=palette_image)
+    palette_image.putpalette(palette_use + tuple(palette_complete[palette]) * (256 - len(palette_use)/3))
+
+    return image.im.convert("P", 1 if dither else 0, palette_image.im)
 
 
 def legofy_gif(base_image, brick_image, output_path, bricks, palette):
@@ -236,7 +276,7 @@ def legofy_image(base_image, brick_image, output_path, bricks, palette):
     new_size = get_new_size(base_image, brick_image, bricks)
 
     if palette:
-        print 'LEGO Palette ' + palette + ' selected...'
+        print 'LEGO Palette ' + palette.title() + ' selected...'
 
     base_image = base_image.convert("RGB")
     if new_size != base_image.size:
