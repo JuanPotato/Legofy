cfb3db22a92c922b900b679cfef341f924c73f70
JuanPotato <hasantiny@gmail.com>
2015-12-02 21:15:31 -0500

    Changed version and added more metadata for pypi

--- setup.py
+++ setup.py
@@ -3,18 +3,23 @@
 from setuptools import setup
 
 setup(
-    name='legofy',
-    version="0.0.1",
-    packages=["legofy"],
+    name="legofy",
+    version="1.0.0",
+    author="Juan Potato",
+    author_email="juanpotatodev@gmail.com",
+    url="https://github.com/JuanPotato/Legofy",
+    description="Make images look as if they are made out of 1x1 LEGO blocks",
+    long_description=("Legofy is a python program that takes a static image or"
+                      " gif and makes it so that it looks as if it was built "
+                      "out of LEGO."),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+    ],
+    license="MIT",
+    packages=['legofy'],
     install_requires=['pillow', 'click'],
     include_package_data=True,
-    maintainer='Juan Potato',
-    description=('Make any image or GIF LEGOFIED!'),
-    url='https://github.com/JuanPotato/Legofy',
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Programming Language :: Python",
-    ],
     entry_points={
         'console_scripts': [
             'legofy = legofy.cli:main',
@@ -23,6 +28,6 @@ setup(
     package_data={
         'bricks': ['*.png'],
     },
-    test_suite='nose.collector',
+    test_suite="nose.collector",
     tests_require=['nose'],
 )
